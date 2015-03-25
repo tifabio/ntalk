0c78ff51a59119611a1f192e157e5d06cdd9a7c1
tifabio <tifabio@gmail.com>
2015-03-25 12:42:48 +0000

    Pagina 50

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require( 'express' )
+  , load    = require( 'express-load' )
+  , app     = express()
+;
+
+app.set( 'views' , __dirname + '/views' );
+app.set( 'view engine' , 'ejs' );
+app.use( express.static ( __dirname + '/public' ));
+
+load( 'models' )
+    .then( 'controllers' )
+    .then( 'routes' )
+    .into( app );
+
+app.listen( process.env.PORT , function() {
+  console.log ( "Ntalk no ar." );
+});
