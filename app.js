edf11e77d3ac38445f6b9203246182605d2d31d3
tifabio <tifabio@gmail.com>
2015-03-27 16:53:22 +0000

    Pagina 64

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express        = require( 'express' ),
     expressSession = require( 'express-session' ),
     bodyParser     = require( 'body-parser' ),
     cookieParser   = require( 'cookie-parser' ),
+    methodOverride = require( 'method-override' ),
     app            = express();
 
 app.set( 'views' , __dirname + '/views' );
@@ -11,6 +12,7 @@ app.use( cookieParser( 'ntalk' ) );
 app.use( expressSession( { secret: 'ntalk', resave: true, saveUninitialized: true } ) );
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded( { extended: true } ) );
+app.use( methodOverride( '_method' ) );
 app.use( express.static ( __dirname + '/public' ) );
 
 expressLoad( 'models' )
--- routes/contatos.js
+++ routes/contatos.js
@@ -1,4 +1,11 @@
 module.exports = function( app ) { 
-    var contatos = app.controllers.contatos; 
-    app.get( '/contatos', contatos.index ); 
+    var autenticar = require('./../middlewares/autenticador'),
+        contatos   = app.controllers.contatos; 
+        
+    app.get( '/contatos', autenticar, contatos.index ); 
+    app.get( '/contato/:id', autenticar, contatos.show ); 
+    app.post( '/contato', autenticar, contatos.create ); 
+    app.get( '/contato/:id/editar', autenticar, contatos.edit ); 
+    app.put( '/contato/:id', autenticar, contatos.update );
+    app.delete( '/contato/:id', autenticar, contatos.destroy ); 
 };
