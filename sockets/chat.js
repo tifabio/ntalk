e734b3f08fe592bfd90d0a96a16c24dc1049bafd
tifabio <tifabio@gmail.com>
2015-04-07 18:15:00 +0000

    Pagina 87

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,12 +1,47 @@
 module.exports = function ( io ) {
-    var sockets = io.sockets;
+    var crypto  = require( 'crypto' ),
+        sockets = io.sockets,
+        onlines = {};
+        
     sockets.on( 'connection', function(client) {
+        var session = client.handshake.session,
+            usuario = session.usuario;
+        
+        onlines[usuario.email] = usuario.email;
+        
+        for( var email in onlines ) {
+            client.emit('notify-onlines', email);
+            client.broadcast.emit('notify-onlines', email);
+        }
+        
+        client.on( 'join', function(sala) {
+            if( !sala ) {
+                var timestamp = new Date().toString(),
+                    md5       = crypto.createHash('md5');
+                    sala      = md5.update(timestamp).digest('hex');
+            }
+            session.sala = sala;
+            client.join( sala );
+        });
+        
         client.on( 'send-server', function(msg) {
-            var session = client.handshake.session,
-                usuario = session.usuario,
+            var sala = session.sala,
+                data = { email: usuario.email, sala: sala },
                 msg = "<b>" + usuario.nome + ":</b> " + msg + "<br>";
-            client.emit( 'send-client', msg );
-            client.broadcast.emit( 'send-client', msg );
+            client.broadcast.emit( 'new-message', data );
+            sockets.in(sala).emit( 'send-client', msg );
+        });
+        
+        client.on( 'disconnect', function() {
+            var sala = session.sala,
+                msg = "<b>" + usuario.nome + ":</b> saiu.<br>";
+            
+            client.broadcast.emit('notify-offlines', usuario.email);
+            sockets.in(sala).emit( 'send-client', msg );
+            
+            delete onlines[usuario.email];
+            
+            client.leave( sala );
         });
     });
 }
