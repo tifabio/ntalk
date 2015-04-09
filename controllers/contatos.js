d75a023f0963b801e9cdf235b2bfa65fbdf0338f
tifabio <tifabio@gmail.com>
2015-04-09 14:12:25 +0000

    Pagina 98

--- controllers/contatos.js
+++ controllers/contatos.js
@@ -1,41 +1,69 @@
 module.exports = function ( app ) {
+    var Usuario = app.models.usuario;
+    
     var ContatosController = {  
         index: function ( req , res ) { 
-            var usuario  = req.session.usuario,
-                contatos = usuario.contatos,
-                params = { usuario: usuario, contatos: contatos };
-            res.render ( 'contatos/index', params );
+            var _id = req.session.usuario._id;
+            Usuario.findById( _id, function( erro, usuario) {
+                var params = { contatos: usuario.contatos };
+                
+                res.render ( 'contatos/index', params );
+            });
         },
         create: function ( req , res ) {
-            var contato = req.body.contato,
-                usuario = req.session.usuario;
-            usuario.contatos.push(contato);
-            res.redirect('/contatos');
+            var _id = req.session.usuario._id;
+            Usuario.findById( _id, function( erro, usuario) {
+                var contato  = req.body.contato;
+                usuario.contatos.push(contato);
+                
+                usuario.save( function() {
+                    res.redirect('/contatos');
+                });
+            });
         },
         show: function ( req , res ) {
-            var id      = req.params.id,
-                contato = req.session.usuario.contatos[id],
-                params  = { contato: contato, id: id };
-            res.render('contatos/show', params );
+            var _id = req.session.usuario._id;
+            Usuario.findById( _id, function( erro, usuario) {
+                var contatoID = req.params.id,
+                    contato   = usuario.contatos.id( contatoID ),
+                    params    = { contato: contato };
+                    
+                res.render('contatos/show', params );
+            });
         },
         edit: function ( req , res ) {
-            var id      = req.params.id,
-                usuario = req.session.usuario,
-                contato = usuario.contatos[id],
-                params  = { usuario: usuario, contato: contato, id: id };
-            res.render('contatos/edit', params );
+            var _id = req.session.usuario._id;
+            Usuario.findById( _id, function( erro, usuario) {
+                var contatoID = req.params.id,
+                    contato   = usuario.contatos.id( contatoID ),
+                    params    = { contato: contato };    
+                    
+                res.render('contatos/edit', params );
+            });
         },
         update: function ( req , res ) {
-            var contato = req.body.contato,
-                usuario = req.session.usuario;
-            usuario.contatos[req.params.id] = contato;
-            res.redirect('/contatos');
+            var _id = req.session.usuario._id;
+            Usuario.findById( _id, function( erro, usuario) {
+                var contatoID = req.params.id,
+                    contato   = usuario.contatos.id( contatoID );
+                contato.nome  = req.body.contato.nome;
+                contato.email = req.body.contato.email;
+                
+                usuario.save( function() {
+                    res.redirect('/contatos');
+                });
+            });
         },
         destroy: function ( req , res ) {
-            var id      = req.params.id,
-                usuario = req.session.usuario;
-            usuario.contatos.splice(id, 1);
-            res.redirect('/contatos');
+            var _id = req.session.usuario._id;
+            Usuario.findById( _id, function( erro, usuario) {
+                var contatoID = req.params.id;
+                usuario.contatos.id(contatoID).remove();
+                
+                usuario.save( function() {
+                    res.redirect('/contatos');
+                });
+            });
         }
     }; 
     return ContatosController;
