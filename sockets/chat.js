e934dce7275799cd8df571efafaa39b7fd15e5b8
Fabio Andrei <tifabio@gmail.com>
2015-09-30 14:20:54 +0000

    Bugfix: Ao entrar na sala as mensagens estavam duplicando

--- sockets/chat.js
+++ sockets/chat.js
@@ -26,12 +26,12 @@ module.exports = function ( io ) {
             
             var msg = "<b>"+usuario.nome+":</b> entrou.<br>";
             
-            redis.lpush( sala, msg, function( erro , res ) {
-                redis.lrange( sala, 0, -1, function( erro , msgs ) {
-                    msgs.forEach( function( msg ) {
-                    	sockets.in( sala ).emit( 'send-client', msg );
-                    });
+            redis.lrange(sala, 0, -1, function(erro, msgs) {
+                msgs.forEach(function(msg) {
+                    client.emit('send-client', msg);
                 });
+                redis.rpush(sala, msg);
+                sockets.in(sala).emit('send-client', msg);
             });
         });
         
@@ -40,7 +40,7 @@ module.exports = function ( io ) {
                 data = { email: usuario.email, sala: sala },
                 msg = "<b>" + usuario.nome + ":</b> " + msg + "<br>";
                 
-            redis.lpush( sala, msg );
+            redis.rpush( sala, msg );
                 
             client.broadcast.emit( 'new-message', data );
             sockets.in(sala).emit( 'send-client', msg );
@@ -50,7 +50,7 @@ module.exports = function ( io ) {
             var sala = session.sala,
                 msg = "<b>" + usuario.nome + ":</b> saiu.<br>";
             
-            redis.lpush( sala, msg );
+            redis.rpush( sala, msg );
             
             client.broadcast.emit('notify-offlines', usuario.email);
             sockets.in(sala).emit( 'send-client', msg );
